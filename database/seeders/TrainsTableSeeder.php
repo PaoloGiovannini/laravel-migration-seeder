f1043f1c20dd8dc4159477673f0aee3c3d038115
Paolo Giovannini <giovannini13@gmail.com>
2023-05-15 15:03:48 +0200

    carico nel database le informazioni dal file csv

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,7 +16,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        self::getCsvContent(__DIR__ . '/trains.csv');
+        $csvData = self::getCsvContent(__DIR__ . '/trains.csv');
+        foreach ($csvData as $index => $row) {
+            if ($index > 0) {
+                $newTrain = new Train();
+                $newTrain->company = $row[0];
+                $newTrain->departure_station = $row[1];
+                $newTrain->arrival_station = $row[2];
+                $newTrain->departure_time = $row[3];
+                $newTrain->arrival_time = $row[4];
+                $newTrain->train_code = $row[5];
+                $newTrain->wagons_number = $row[6];
+                $newTrain->on_time = $row[7];
+                $newTrain->cancelled = $row[8];
+                $newTrain->save();
+            }
+        }
     }
 
     public static function getCsvContent(string $path)
